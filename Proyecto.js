c201f66d06286bf20085e7d12d9d77165ff01f03
ooldigital0 <ooldigital0@gmail.com>
2024-01-08 23:04:08 -0300

    agregué arrays

--- Proyecto.js
+++ Proyecto.js
@@ -1,10 +1,11 @@
+// Array para almacenar los precios finales
+var preciosFinales = [];
+
 function calcularValorFinal() {
-    // Obtener el precio, impuesto y descuento mediante prompt()
     var precio = parseFloat(prompt("Ingrese el precio del producto"));
     var impuesto = parseFloat(prompt("Ingrese el impuesto en porcentaje"));
     var descuento = parseFloat(prompt("Ingrese el descuento en porcentaje"));
 
-    // Función para calcular el precio final
     function calcularPrecioFinal(precio, impuesto, descuento) {
         var impuestoAplicado = precio * (impuesto / 100);
         var descuentoAplicado = precio * (descuento / 100);
@@ -12,16 +13,23 @@ function calcularValorFinal() {
         return precioFinal.toFixed(2);
     }
 
-    // Validar el precio y calcular el precio final
     var mensaje = '';
     if (precio <= 0) {
         mensaje = 'El precio del producto debe ser mayor que cero.';
     } else {
         var precioFinal = calcularPrecioFinal(precio, impuesto, descuento);
         mensaje = 'El valor final del producto es: $' + precioFinal;
+
+        // Agregar el precio final al array de preciosFinales
+        preciosFinales.push(parseFloat(precioFinal));
     }
 
-    // Mostrar el resultado utilizando alert() y console.log()
     alert(mensaje);
     console.log(mensaje);
+}
+
+// Función para mostrar los precios finales guardados en el array
+function mostrarPreciosFinales() {
+    console.log("Precios Finales:");
+    console.log(preciosFinales);
 }
